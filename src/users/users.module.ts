f27cd747259871bb93a364034b4e47bb10d4c2bb
Maik <maikpla2005@gmail.com>
2026-03-11 01:52:29 +0700

    forogo to inject ts with the jwt module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]),
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_ACCESS_SECRET'),
+      signOptions: {
+        expiresIn: 15 * 60, // 15 minutes in seconds
+      },
+    }),
+    inject: [ConfigService],
+  }),],
   providers: [UsersService],
   exports: [UsersService],
   controllers: [UsersController]
+
 })
 export class UsersModule { }
